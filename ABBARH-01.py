47d875be50dd3985dc7406637e5752161fd8f40a
scientisz <81861106+scientisz@users.noreply.github.com>
2021-04-05 11:37:47 +0100

    Add files via upload

---
+++ ABBARH-01.py
@@ -0,0 +1,30 @@
+print('Question ONE')
+NAME='IBRAHIM MUHAMMAD AWWAL'
+DEPT='COMPUTER SCIENCE DEPARTMENT'
+print('my name is',NAME,'i am from',DEPT)
+print('Question TWO')
+scare='Boo!'
+
+print(scare)
+print('Question THREE')
+NumberA=7
+NumberB=4
+NumberC=NumberA + NumberB
+print(NumberA,NumberB,NumberC)
+print('Question FOUR')
+Divide=12/0.5
+print(Divide)
+print('Question FIVE')
+Add=5.1 + 6
+print(Add)
+print('Question SIX')
+ABBA=8%3
+print(ABBA)
+print('Question SEVEN')
+ResultA=2 * 4
+
+ResultB=4 + 2
+
+ResultC=ResultA * ResultB
+
+print(ResultC)
